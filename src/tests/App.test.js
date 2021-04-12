43aae8cc1c0fe55d57a0283fc322368efaaac35f
holly.wilcock <hollywilcock99@gmail.com>
2021-04-12 21:08:25 +0100

    file structure updates

---
+++ src/tests/App.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../components/App";
+
+test("renders h1 element", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Weather App");
+  expect(linkElement).toBeInTheDocument();
+});
