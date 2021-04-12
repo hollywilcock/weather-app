2a1fb23b647150975257e4746f340fafd07269fe
holly.wilcock <hollywilcock99@gmail.com>
2021-04-12 20:20:11 +0100

    app set up complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-      <div className="App">
+    <div className="App">
       <h1>Weather App </h1>
     </div>
   );
